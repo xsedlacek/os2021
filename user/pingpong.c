b397aa572fa8c5bd3464ecdaca306b1e4f1b2fe9
Peter Sedlacek <xsedlacekp@stuba.sk>
2021-09-28 13:10:15 +0200

    feat:implement pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,28 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc,char*argv[])
+{
+  int to_parent[2];
+  int to_child[2];
+
+  pipe(to_parent);
+  pipe(to_child);
+
+  int pid = fork();
+
+  if(pid == 0){
+    char received;
+    read(to_child[0],&received,1);
+    printf("%d: ping received\n",getpid());
+    write(to_parent[1],"x",1);
+  } else {
+    write(to_child[1],"b",1);
+    char received;
+    read(to_parent[0],&received,1);
+    printf("%d: pong received\n",getpid());
+  }
+  exit(0);
+}
+
